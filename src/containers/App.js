c3af515563c6f158ff343b8fec55ff7ee758a688
shirleen <shirleenoey@gmail.com>
2023-03-13 21:26:18 +0700

    updating files, deploy to git hub pages

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,8 +2,8 @@ import React, {Component} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css'
-
 
 class App extends Component {
 	constructor() {
@@ -36,8 +36,10 @@ class App extends Component {
     			<h1 className='f1'>RoboFriends</h1>
     			<SearchBox searchChange={this.onSearchChange}/>
     			<Scroll>
-    				<CardList robots={filteredRobots}/>
-    			</Scroll>
+            <ErrorBoundry>
+    				  <CardList robots={filteredRobots}/>
+            </ErrorBoundry>
+          </Scroll>
     		</div>
     		);
 		}
